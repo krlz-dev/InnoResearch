4ffdcbb35bf894dd81604fbc3ba0b1a707e2749f
carlos <soy_solamente@hotmail.com>
2020-03-16 22:49:18 +0300

    add models

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -9,6 +9,6 @@ const Header = () => {
             </div>
         </div>
     );
-}
+};
 
 export default Header;
--- app/src/components/Questionnaire.js
+++ app/src/components/Questionnaire.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react'
-
+import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import "./css/Questionnaire.css"
+import QuestionnaireResource from '../fhir/Questionnaire';
 
 const Questionnaire = () => {
 
@@ -12,37 +13,30 @@ const Questionnaire = () => {
 
     return (
         <div className={"Card"}>
-            <h3>BMI calculation</h3>
-            <TextField label="Height" onChange={event => setHeight(event.target.value)}/> <br/> <br/>
-            <TextField label="Weight" onChange={event => setWeight(event.target.value)}/><br/> <br/> <br/>
-            <Button variant="contained" color="primary"
+            <h3>{QuestionnaireResource.title}</h3>
+            <TextField label={QuestionnaireResource.item[0].text} type="decimal"
+                       onChange={event => setHeight(event.target.value)}/>
+
+            <br/><br/>
+            <TextField label={QuestionnaireResource.item[1].text}
+                       onChange={event => setWeight(event.target.value)}/>
+            <br/>
+            <br/>
+            <br/>
+            <Button className={"t3"} variant="contained" color="primary"
                     onClick={async () => setBmiResult(await calculate_bmi(height, weight))}>Calculate</Button>
-            <br/> <br/>
+            <br/>
+
             <p> Your BMI is: {bmiResult}</p>
         </div>
     )
 };
 
 const calculate_bmi = async (height, weight) => {
-
     const data = {
-        resourceType: 'QuestionnaireResponse',
+        resourceType: "QuestionnaireResponse",
+        questionnaire: QuestionnaireResource.id,
         id: 'QuestionnaireResponse-26ed7ef3-25b1-4e4a-9e33-7c2c7030f25b',
-        meta: {
-            profile: [
-                'http://Url.profile'
-            ],
-            tag: [
-                {
-                    code: 'PATIENT-ID-86893428934234',
-                    system: 'http://Url.patients'
-                }
-            ]
-        },
-        identifier: {
-            system: 'https://portavita.nl/questionnaire/questionnaire-response-identifier',
-            value: 'QuestionnaireResponse-26ed7ef3-25b1-4e4a-9e33-7c2c7030f25b'
-        },
         status: 'generated',
         authored: 'Fri Nov 29 2019 18:53:08 GMT+0300 (Moscow Standard Time)',
         item: [
@@ -74,6 +68,7 @@ const calculate_bmi = async (height, weight) => {
         },
         body: JSON.stringify(data),
     });
+
     const json = await response.json();
     return json.valueQuantity.value
 };
